c3293c69e8d75e6c7a93100616e434cf3acf84f4
Vivian Yim <keroviv86@gmail.com>
2022-01-05 21:12:37 -0600

    commit tree 65cbb55b677c4a006ba595f6a07da8b6e3134de9

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    return str.replaceAll(/[^A-z0-9' -]|\^/g, '')
+  }
+
+  static titleize(str) {
+    
+  }
 }
